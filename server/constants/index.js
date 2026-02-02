2b90b1c38a6872ac3dafd9ac3ab163fc2abeb54e
Syeda Maham Amjad <163040767+maham719@users.noreply.github.com>
2026-02-03 03:16:06 +0500

    Refactor environment variable export structure

--- server/constants/index.js
+++ server/constants/index.js
@@ -1,5 +1,8 @@
-import "dotenv/config"
+import dotenv from "dotenv"
+dotenv.config()
 
-const ENV=process.env
-
-export default ENV
+export default {
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME
+}
