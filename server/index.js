9c8a20977987dcb18df2a332466341161bdb7ab5
syeda maham amjad <zohameer96@gmail.com>
2026-02-03 02:39:18 +0500

    update

--- server/index.js
+++ server/index.js
@@ -1,24 +1,41 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import cors  from 'cors'
+import cors from 'cors'
 import router from './routes/index.js'
-const app=express()
 import ENV from './constants/index.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/",router)
+// API routes
+app.use("/", router)
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../todolist/dist")))
+
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../todolist/dist', 'index.html'))
+})
 
 mongoose.connect(`mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASSWORD}@mahamcluster.hkmzvsj.mongodb.net/${ENV.DB_NAME}?appName=mahamcluster`)
 
-mongoose.connection.on('error', (err)=>{
-console.log("db is not connected")
-})
-mongoose.connection.on("open", ()=>{
-console.log("db connected")
+mongoose.connection.on('error', (err) => {
+  console.log("db is not connected", err.message)
 })
 
-app.listen(3001,()=>{
-    console.log("running on port 3001")
+mongoose.connection.on("open", () => {
+  console.log("db connected")
+})
+
+app.listen(3001, () => {
+  console.log("running on port 3001")
 })
--- server/modules/tasks/db/put.js
+++ server/modules/tasks/db/put.js
@@ -1,7 +1,12 @@
 import TodoModel from "../model/index.js"
 
-const updateTodoInDb =(id,res)=>{
-TodoModel.findByIdAndUpdate({_id:id},{done:true})
+const updateTodoInDb =async(id,res)=>{
+const todo=await TodoModel.findByIdAndUpdate({_id:id})
+const updatedTodo = await TodoModel.findByIdAndUpdate(
+      id,
+      { done: !todo.done },
+      { new: true }
+    )
 .then(result=>res.json(result))
 .catch(err=>res.json(err))
 }
